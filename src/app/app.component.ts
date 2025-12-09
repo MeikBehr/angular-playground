ccd8c1f52da3360188f5f96ae26379865d8f666a
Meik Behr <meik.behr@gmx.de>
2025-12-09 12:59:55 +0100

    feat: add fundamentals page and fix routing setup
    - Added standalone FundamentalsComponent under /pages
    - Registered /fundamentals route in app.routes.ts
    - Cleaned up app.component.ts to remove incorrect routing export
    - Kept AppComponent focused on layout (header, router-outlet, footer)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-
 
 @Component({
   selector: 'app-root',
@@ -17,7 +14,3 @@ import { HomeComponent } from './pages/home/home.component';
 export class AppComponent {
   title = 'angular-playground';
 }
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent},
-];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FundamentalsComponent } from './pages/fundamentals/fundamentals.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'fundamentals', component: FundamentalsComponent},
 ];
