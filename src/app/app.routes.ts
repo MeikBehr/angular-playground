0c739e45cf8611f81c5c25c8e9a92a12d9d43993
Meik Behr <meik.behr@gmx.de>
2025-12-11 14:13:35 +0100

    feat(directives): create directives component, register route and add initial template-based page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FundamentalsComponent } from './pages/fundamentals/fundamentals.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'fundamentals', component: FundamentalsComponent},
+  { path: 'directives', component: DirectivesComponent},
 ];
