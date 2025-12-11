c4803d4d71dc1e5d49f2b188c75a7187be406074
Meik Behr <meik.behr@gmx.de>
2025-12-11 17:50:42 +0100

    feat(directives): add functional *ngFor examples with live preview and code display
    - implemented table rendering using *ngFor with mock data
    - added character iteration example using ('Angular'.split(''))
    - introduced code preview sections using <pre [innerText]="..."> for safe display
    - structured layout using flex-row and showCode containers for side-by-side demonstration
    - improved learning clarity by pairing each example with its rendered output and source code

--- src/app/pages/directives/directives.component.ts
+++ src/app/pages/directives/directives.component.ts
@@ -11,9 +11,39 @@ import { CommonModule } from '@angular/common';
 export class DirectivesComponent {
 
     // ngif
-    display: boolean = true;
-    toggleDisplay() {
-      this.display = !this.display;
+    displayNgIf: boolean = true;
+    toggleDisplayNgif() {
+      this.displayNgIf = !this.displayNgIf;
     }
+
+    // ngFor
+    ngForObject = [
+      {id: 1, name : 'GPT', 'developer':'OpenAI'},
+      {id: 2, name : 'Gemini', 'developer':'Google'},
+      {id: 3, name : 'Claude', 'developer':'Anthropic'},
+      {id: 4, name : 'Perplexity', 'developer':'Perplexity AI'},
+      {id: 5, name : 'Llama', 'developer':'Meta'},
+    ]
+
+    codeExample = `
+    <table>
+      <tr>
+        <th>Nummer</th>
+        <th>KI / LLM</th>
+        <th>Anbieter</th>
+      </tr>
+      <tr *ngFor="let entry of ngForObject">
+        <td>{{ entry.id }}</td>
+        <td>{{ entry.name }}</td>
+        <td>{{ entry.developer }}</td>
+      </tr>
+    </table>
+    `;
+
+    codeExampleString = `
+    <span *ngFor="let char of ('Angular'.split(''))">
+      {{ char }} -
+    </span>
+    `;
 
 }
