f43dd448e581487e5bfcce455b31d740d9f8c0fa
Meik Behr <meik.behr@gmx.de>
2025-12-11 15:37:29 +0100

    feat(directives): add *ngIf section including example logic - added description of directives - added first completed directive section (*ngIf) including description and sample logic

--- src/app/pages/directives/directives.component.ts
+++ src/app/pages/directives/directives.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.scss'
 })
 export class DirectivesComponent {
+
+    // ngif
+    display: boolean = true;
+    toggleDisplay() {
+      this.display = !this.display;
+    }
 
 }
