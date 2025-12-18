2c6b5036fbaa4905cedc38d71835ac26feb0b96a
Meik Behr <meik.behr@gmx.de>
2025-12-18 17:45:41 +0100

    feat(templates): add templates page with initial structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FundamentalsComponent } from './pages/fundamentals/fundamentals.component';
 import { DirectivesComponent } from './pages/directives/directives.component';
+import { TemplatesComponent } from './pages/templates/templates.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'fundamentals', component: FundamentalsComponent},
   { path: 'directives', component: DirectivesComponent},
+  { path: 'templates', component: TemplatesComponent},
 ];
