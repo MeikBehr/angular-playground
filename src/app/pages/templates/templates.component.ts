b6d9ff376a5e95e0d52fbc7514dd98d33267395c
Meik Behr <meik.behr@gmx.de>
2025-12-18 22:35:55 +0100

    feat(templates): add ng-template section with conditional example and explanation

--- src/app/pages/templates/templates.component.ts
+++ src/app/pages/templates/templates.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-templates',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './templates.component.html',
   styleUrl: './templates.component.scss'
 })
 export class TemplatesComponent {
 
+    // ng-container
 ngContainerWithout = `
 <div>
   <p>Dieses DIV</p>
@@ -24,5 +27,20 @@ ngContainerWith = `
 </ng-container>
 `;
 
+  // ng-template
+  user: string = '';
 
+ngTemplateCode = `
+<div class="input-container">
+  <input id="templateinput" type="text" [(ngModel)]="user" />
+  @if (user == '') { <label for="templateinput">Name</label> }
+</div>
+<ng-container *ngIf="user !== ''; else notValid"></ng-container>
+<ng-template #notValid>
+  <h4>Bitte Namen eingeben!</h4>
+</ng-template>
+
+Der <ng-container> erzeugt kein DOM-Element und dient hier nur als Host für *ngIf mit else. 
+Wenn user === '', wird das ng-template mit der Referenz #notValid gerendert.
+`;
 }
