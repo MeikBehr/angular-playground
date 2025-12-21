60a5109fe179178e3b028aeac86d3bd4dd1b1462
Meik Behr <meik.behr@gmx.de>
2025-12-21 13:23:16 +0100

    feat(templates): add ngTemplateOutlet examples with implicit and named context

--- src/app/pages/templates/templates.component.ts
+++ src/app/pages/templates/templates.component.ts
@@ -43,4 +43,80 @@ ngTemplateCode = `
 Der <ng-container> erzeugt kein DOM-Element und dient hier nur als Host für *ngIf mit else. 
 Wenn user === '', wird das ng-template mit der Referenz #notValid gerendert.
 `;
+
+  // ng-TemplateOutlet
+
+  cards = [
+    {
+      name: 'ChatGPT',
+      model: 'o1 / GPT-4o',
+      type: 'LLM',
+      cost: '20 $',
+      rating: '10/10'
+    },
+    {
+      name: 'Gemini',
+      model: '2.0 Flash/Pro',
+      type: 'Multimodal LLM',
+      cost: '21 €',
+      rating: '9/10'
+    },
+    {
+      name: 'Claude',
+      model: '3.5 Sonnet',
+      type: 'LLM / Coding',
+      cost: '20 $',
+      rating: '9/10'
+    },
+    {
+      name: 'Llama',
+      model: '4.0',
+      type: 'Open Source LLM',
+      cost: '0 €',
+      rating: '9/10'
+    }
+  ];
+
+
+ngTemplateOutletImplizit: string = `
+<ng-template #aiTable let-ai>
+  <table>
+    <tbody>
+      @for (e of ai; track $index) {
+        <tr>
+          <th>{{ e.name}}</th>
+          <th>{{ e.model}}</th>
+          <th>{{ e.type}}</th>
+          <th>{{ e.cost}}</th>
+          <th>{{ e.rating}}</th>
+        </tr>
+      }
+    </tbody>
+  </table>
+</ng-template>
+<ng-container 
+  *ngTemplateOutlet="aiTable; context: {$implicit: cards}">
+</ng-container>
+`;
+
+ngTemplateOutletContext: string = `
+<ng-template #aiTableNamed let-ai="ai">
+  <table>
+    <tbody>
+      @for (e of ai; track $index) {
+        <tr>
+          <th>{{ e.name}}</th>
+          <th>{{ e.model}}</th>
+          <th>{{ e.type}}</th>
+          <th>{{ e.cost}}</th>
+          <th>{{ e.rating}}</th>
+        </tr>
+      }
+    </tbody>
+  </table>
+</ng-template>
+<ng-container  
+  *ngTemplateOutlet="aiTableNamed; context: { ai: cards }">
+</ng-container>
+`;
 }
